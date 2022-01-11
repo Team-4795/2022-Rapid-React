75905bc6a0e30865c9cdf171d3b643ef0c38df7d
Amalan Iyengar <amalan.iyengar@gmail.com>
2022-01-11 11:50:06 -0500

    Clean drivebase branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,7 @@ package frc.robot;
 import edu.wpi.first.math.util.Units;
 
 public final class Constants {
-  public final class DrivebaseConstants {
-    public static final int LEFT_LEADER = 1;
-    public static final int LEFT_FOLLOWER = 2;
-    public static final int RIGHT_LEADER = 3;
-    public static final int RIGHT_FOLLOWER = 4;
-  }
-
+ 
   public final static class VisionConstants {
     public static final String CAMERA_NAME = "mmal_service_16.1";
 
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,31 +9,14 @@ import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.Constants.ControllerConstants;
-import frc.robot.commands.DrivebaseTeleop;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.Drivebase;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Vision;
+
 
 public class RobotContainer {
-  private final Drivebase drivebase = new Drivebase();
-  private final Intake intake = new Intake();
-  private final Shooter shooter = new Shooter();
-  private final Climber climber = new Climber();
-  private final Vision vision = new Vision();
   
   private final XboxController controller = new XboxController(ControllerConstants.CONTROLLER_PORT);
 
   public RobotContainer() {
-    drivebase.setDefaultCommand(new DrivebaseTeleop(
-      drivebase,
-      () -> applyDeadband(-controller.getLeftY()),
-      () -> applyDeadband(-controller.getRightX()),
-      () -> applyDeadband(controller.getRightTriggerAxis())
-    ));
-
-    configureButtonBindings();
+     configureButtonBindings();
   }
 
   private void configureButtonBindings() {}
@@ -47,17 +30,5 @@ public class RobotContainer {
     controller.setRumble(RumbleType.kRightRumble, rumble);
   }
 
-  private double applyDeadband(double value) {
-    double deadband = ControllerConstants.JOYSTICK_DEADBAND;
-
-    if (Math.abs(value) > deadband) {
-      if (value > 0.0) {
-        return (value - deadband) / (1.0 - deadband);
-      } else {
-        return (value + deadband) / (1.0 - deadband);
-      }
-    } else {
-      return 0.0;
-    }
-  }
+ 
 }
