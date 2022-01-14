86c74b75c8b47c2f6db99c5102b447f91a897c4c
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2022-01-14 15:25:43 -0500

    Update Drivebase.java
    add all encoder exposures
    
    - [x] Get wheel speeds
    - [x] Average Encoder Distance
    - [x] Set ticks per revolution (Right)
    - [x] Set ticks per revolution (Left)
    - [x] Reset Encoders
    - [x] Get Start Distance (Left)
    - [x] Get Start Distance (Right)
    - [x] Get Distance (Right)
    - [x] Get Distance (Left)
    - [x] Init Right Encoder
    - [x] Init Left Encoder

--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivebaseConstants;
@@ -19,15 +21,48 @@ public class Drivebase extends SubsystemBase {
   private CANSparkMax rightLeader = new CANSparkMax(DrivebaseConstants.RIGHT_LEADER, MotorType.kBrushless);
   private CANSparkMax rightFollower = new CANSparkMax(DrivebaseConstants.RIGHT_FOLLOWER, MotorType.kBrushless);
 
+  // The left-side drive encoder
+  Encoder leftEncoders = new Encoder(DrivebaseConstants.LEFT_LEADER, DrivebaseConstants.LEFT_FOLLOWER, false, Encoder.EncodingType.k2X);
+
+  // The right-side drive encoder
+  Encoder rightEncoders = new Encoder(DrivebaseConstants.RIGHT_LEADER, DrivebaseConstants.RIGHT_FOLLOWER, false, Encoder.EncodingType.k2X);
+
+  private double leftEncoderStart, rightEncoderStart;
+
   private DifferentialDrive diffDrive = new DifferentialDrive(leftLeader, rightLeader);
 
   public Drivebase() {
     leftFollower.follow(leftLeader);
     rightFollower.follow(rightLeader);
+
+    // Configures the encoder to return a distance of 4 for every 256 pulses
+    // Also changes the units of getRate  
+    leftEncoders.setDistancePerPulse(4./256.);
+    rightEncoders.setDistancePerPulse(4./256.);
+
+    leftEncoderStart = leftEncoders.getDistance();
+    rightEncoderStart = rightEncoders.getDistance();
   }
 
   public void curvatureDrive(double speed, double rotation, boolean quickTurn) {
     diffDrive.curvatureDrive(speed, rotation, quickTurn);
+  }
+
+  //ENCODER STUFF
+  double getRightWheelDistance = rightEncoders.getDistance();
+  double getLeftWheelDistance = leftEncoders.getDistance();
+
+  public void resetEncoders() {
+    rightEncoders.reset();
+    leftEncoders.reset();
+  }
+
+  public double getAverageEncoderDistance() {
+    return (leftEncoders.getDistance() + rightEncoders.getDistance()) / 2.0;
+  }
+
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(leftEncoders.getRate(), rightEncoders.getRate());
   }
 
   public void reverse() {}
