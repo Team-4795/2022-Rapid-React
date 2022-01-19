5bf770d4813493e060d69614aad7fd84229b65c4
clairet0 <97416315+clairet0@users.noreply.github.com>
2022-01-18 20:30:15 -0500

    Update PneumaticsSubsystem.java

--- src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
@@ -6,12 +6,23 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class PneumaticsSubsystem extends SubsystemBase {
-  DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH,forwardChannel,reverseChannel );
+  /** new double solenoid */
+  private DoubleSolenoid mySolenoid = new DoubleSolenoid(1,2); //forward channel, backward channel
   
-  public PneumaticsSubsystem() {
+  public PneumaticsSubsystem() {}
+  
+  public void forward() { 
+    mySolenoid.set(Value.kForward);
+  }
+  public void back() {
+    mySolenoid.set(Value.kReverse);
+  }
+  public void off() {
+    mySolenoid.set(Value.kOff);
   }
 
   @Override
