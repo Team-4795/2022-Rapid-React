286a66d428fb46ad59705f99cc07a7af50ef8078
clairet0 <97416315+clairet0@users.noreply.github.com>
2022-01-19 11:19:08 -0500

    corrected spelling of solenoid

--- src/main/java/frc/robot/commands/PneumaticsCommand.java
+++ src/main/java/frc/robot/commands/PneumaticsCommand.java
@@ -5,15 +5,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Soleniod;
+import frc.robot.subsystems.Solenoid;
 
 public class PneumaticsCommand extends CommandBase {
-  private final Soleniod soleniod;
+  private final Solenoid solenoid;
   /** Create new pneumatics commands. */
-  public PneumaticsCommand(Soleniod soleniod) {
+  public PneumaticsCommand(Solenoid solenoid) {
     // Use addRequirements() here to declare subsystem dependencies.
-    this.soleniod = soleniod;
-    addRequirements(intake);
+    this.solenoid = solenoid;
+    addRequirements(solenoid);
   }
 
   // Called when the command is initially scheduled.
@@ -23,13 +23,13 @@ public class PneumaticsCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    soleniod.forward();
+    solenoid.forward();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    soleniod.off();
+    solenoid.off();
   }
 
   // Returns true when the command should end.
