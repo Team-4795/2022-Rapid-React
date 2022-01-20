9142559642d9139a595a95330fa68563a56e1f00
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2022-01-20 18:33:02 -0500

    Update RobotContainer.java, clear sticky faults

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -19,9 +20,12 @@ public class RobotContainer {
   
   private final XboxController controller = new XboxController(ControllerConstants.CONTROLLER_PORT);
 
+  private final PowerDistribution PDP = new PowerDistribution();
+
   public RobotContainer() {
     drivebase.setDefaultCommand(new curveDrive(drivebase, () -> controller.getRawAxis(ControllerConstants.SPEED_JOYSTICK), () -> controller.getRawAxis(ControllerConstants.ROTATION_JOYSTICK), () -> controller.getRawAxis(ControllerConstants.THROTTLE_TRIGGER)));
-     configureButtonBindings();
+    PDP.clearStickyFaults();
+    configureButtonBindings();
   }
 
   private void configureButtonBindings() {
