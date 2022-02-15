a0ec4bc0043d6563bae8128cbccd0f5fa60aa54f
Ryan Huang <97416315+clairet0@users.noreply.github.com>
2022-02-15 18:07:01 -0500

    Combined indexer and color sensors

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -4,61 +4,37 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.sensors.ColorSensor;
+import frc.robot.sensors.ColorSensorV2;
 import frc.robot.Constants.IndexerConstants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 
 public class Indexer extends SubsystemBase {
-  
   private CANSparkMax upperMotor = new CANSparkMax(IndexerConstants.INDEXER_UPPER, MotorType.kBrushless);
   private CANSparkMax lowerMotor = new CANSparkMax(IndexerConstants.INDEXER_LOWER, MotorType.kBrushed);
-
-  PIDController pid = new PIDController(IndexerConstants.kP, IndexerConstants.kI, IndexerConstants.kD);
- 
-  private double targetVelocity = 0;
-
-  private RelativeEncoder m_upperEncoder;
+  private ColorSensorV2 colorSensorV2 = new ColorSensorV2();
+  private ColorSensor colorSensorV3 = new ColorSensor();
 
   public Indexer() {  
-
     upperMotor.restoreFactoryDefaults();
     lowerMotor.restoreFactoryDefaults();
 
-    upperMotor.setIdleMode(IdleMode.kCoast);
-    lowerMotor.setIdleMode(IdleMode.kCoast);
+    upperMotor.setIdleMode(IdleMode.kBrake);
+    lowerMotor.setIdleMode(IdleMode.kBrake);
 
-    m_upperEncoder = upperMotor.getEncoder();
-  }
-
-  public double getUpperEncoder() {
-    return m_upperEncoder.getVelocity();
+    upperMotor.enableVoltageCompensation(12);
+    lowerMotor.enableVoltageCompensation(12);
   }
 
   public void setIndexerSpeed(double upperSpeed, double lowerSpeed) {
-    targetVelocity = upperSpeed;
-    lowerMotor.set(lowerSpeed);
-  }
-/*
-  public void setIndexerPower(double upperSpeed, double lowerSpeed) {
     upperMotor.set(upperSpeed);
     lowerMotor.set(lowerSpeed);
   }
 
-  public void setIndexerSpeed(double upperSpeed, double lowerSpeed) {
-    if (getUpperEncoder() >= upperSpeed){
-      setIndexerPower(0, lowerSpeed);
-    } else {
-      setIndexerPower(upperSpeed, lowerSpeed);
-    }
-  }
-*/
   @Override
-  public void periodic() {
-    upperMotor.set(pid.calculate(m_upperEncoder.getVelocity(), targetVelocity));
-  }
+  public void periodic() {}
 }
