755a1158672a7e2ca7d4a413bb98afabbf882fc6
JakeBoggs <jake.boggs.code@gmail.com>
2022-04-19 17:27:37 -0400

    Additional climber fix

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -83,8 +83,10 @@ public class Climber extends SubsystemBase {
 
   @Override
   public void periodic() {
-    if (!hasZeroed) climb_motor.set(-0.2);
+    if (!hasZeroed) {
+      climb_motor.set(-0.2);
 
-    if (Math.abs(climb_motor.get()) > 0 && climb_encoder.getVelocity() < 2) resetEncoder();
+      if (Math.abs(climb_motor.get()) > 0 && climb_encoder.getVelocity() < 2) resetEncoder();
+    }
   }
 }
