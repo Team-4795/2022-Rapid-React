708450d3d71dbb653acf45500e43620e7d06b331
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2022-06-29 18:10:19 -0400

    remove operator controls

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -124,12 +124,6 @@ public class Robot extends TimedRobot {
     } else {
       robotContainer.setDriverRumble(0);
     }
-
-    if (intake.hasBall()) {
-      robotContainer.setOperatorRumble(0.5);
-    } else {
-      robotContainer.setOperatorRumble(0);
-    }
   }
 
   @Override
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,14 +10,11 @@ import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivebase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.CurvatureDrive;
 import frc.robot.commands.GoalTracker;
-import frc.robot.commands.PrepareShot;
 import frc.robot.commands.Shoot;
 import frc.robot.commands.BallManager;
 import frc.robot.subsystems.Superstructure;
@@ -34,7 +31,6 @@ public class RobotContainer {
   private final BallManager ballManager;
 
   private final Controller driverController = new Controller(ControllerConstants.DRIVER);
-  private final Controller operatorController = new Controller(ControllerConstants.OPERATOR);
 
   public RobotContainer() {
     drivebase = new Drivebase();
@@ -72,33 +68,10 @@ public class RobotContainer {
 
     final JoystickButton highShot = new JoystickButton(driverController, Controller.Button.kY.value);
 
-    final JoystickButton unjamButton = new JoystickButton(operatorController, Controller.Button.kA.value);
-    final JoystickButton intakeOverride = new JoystickButton(operatorController, Controller.Button.kB.value);
-    final JoystickButton resetClimber = new JoystickButton(operatorController, Controller.Button.kX.value);
-    final JoystickButton manualRetract = new JoystickButton(operatorController, Controller.Button.kY.value);
-    final Trigger reverseIntake = new Trigger(() -> operatorController.getRightTriggerAxis() > 0);
-    final Trigger tiltClimber = new Trigger(() -> operatorController.getPOV() == 0);
-    final Trigger untiltClimber = new Trigger(() -> operatorController.getPOV() == 180);
-    final JoystickButton extendClimber = new JoystickButton(operatorController, Controller.Button.kRightBumper.value);
-    final JoystickButton retractClimber = new JoystickButton(operatorController, Controller.Button.kLeftBumper.value);
-
     reverseButton.whenPressed(drivebase::reverse);
     lowShot.whileHeld(new Shoot(drivebase, superstructure, vision, new ShooterPreset(1500, 700, 0)));
     highShot.whileHeld(new Shoot(drivebase, superstructure, vision, new ShooterPreset(1800, 1200, 0)));
     intakeButton.whenPressed(superstructure.intake::toggle);
-
-    unjamButton.whileHeld(new RunCommand(() -> {
-      superstructure.indexer.setIndexerSpeed(-0.3, -1);
-      superstructure.shooter.setShooterPower(-1, -0.3);
-    }, superstructure));
-    intakeOverride.whenPressed(superstructure.intake::toggle);
-    reverseIntake.whenActive(() -> ballManager.setIntakeReversed(true)).whenInactive(() -> ballManager.setIntakeReversed(false));
-    resetClimber.whenPressed(climber::resetEncoder);
-    manualRetract.whileHeld(new RunCommand(() -> climber.setPower(-0.2), climber));
-    tiltClimber.whenActive(climber::tilt);
-    untiltClimber.whenActive(climber::untilt);
-    extendClimber.whileHeld(new RunCommand(climber::extend, climber));
-    retractClimber.whileHeld(new RunCommand(climber::retract, climber));
   }
 
   public Command getAutonomousCommand() {
@@ -108,10 +81,5 @@ public class RobotContainer {
   public void setDriverRumble(double rumble) {
     driverController.setRumble(RumbleType.kLeftRumble, rumble);
     driverController.setRumble(RumbleType.kRightRumble, rumble);
-  }
-
-  public void setOperatorRumble(double rumble) {
-    operatorController.setRumble(RumbleType.kLeftRumble, rumble);
-    operatorController.setRumble(RumbleType.kRightRumble, rumble);
   }
 }
